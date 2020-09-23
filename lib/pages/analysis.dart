8a196db5ccab17bd792eb8e48944d29e72231266
barbarasamson <barbarasamson@gmail.com>
2020-09-23 14:38:37 -0400

    navigation works from all 3 to all 3

--- lib/pages/analysis.dart
+++ lib/pages/analysis.dart
@@ -3,6 +3,20 @@ import 'package:flutter/material.dart';
 class AnalysisScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    void _onItemTapped(int index) {
+      switch(index) {
+        case 1:
+          Navigator.pop(context);
+          break;
+        case 0:
+          Navigator.pushReplacementNamed(context, '/settings');
+          break;
+        case 2:
+        default:
+      }
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Second Screen"),
@@ -14,6 +28,24 @@ class AnalysisScreen extends StatelessWidget {
           },
           child: Text('Go back!'),
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 2,
+        items: [
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.settings),
+            title: new Text("Settings"),
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.apps),
+            title: new Text("Play"),
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.all_inclusive),
+            title: new Text("Analysis"),
+          )
+        ],
+        onTap: _onItemTapped,
       ),
     );
   }
--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -3,6 +3,20 @@ import 'package:flutter/material.dart';
 class SettingsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    void _onItemTapped(int index) {
+      switch(index) {
+        case 1:
+          Navigator.pop(context);
+          break;
+        case 2:
+          Navigator.pushReplacementNamed(context, '/analysis');
+          break;
+        case 0:
+        default:
+      }
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Second Screen"),
@@ -14,6 +28,24 @@ class SettingsScreen extends StatelessWidget {
           },
           child: Text('Go back!'),
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+        items: [
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.settings),
+            title: new Text("Settings"),
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.apps),
+            title: new Text("Play"),
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.all_inclusive),
+            title: new Text("Analysis"),
+          )
+        ],
+        onTap: _onItemTapped,
       ),
     );
   }
